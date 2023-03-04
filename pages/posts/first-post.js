eff56964612d33263cb7e43a4b6fa24ba8c34a44
0ustadh <delatorreivanvincent19@gmail.com>
2023-03-04 21:37:32 +0800

    Deployment tutorial

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,3 +1,4 @@
+/*
 import Link from 'next/link';
 import Head from 'next/head';
 import Script from 'next/script';
@@ -23,4 +24,19 @@ export default function FirstPost() {
         </h2>
       </Layout>
     );
+}
+*/
+
+export function getPostData(id) {
+  const fullPath = path.join(postsDirectory, `${id}.md`);
+  const fileContents = fs.readFileSync(fullPath, 'utf8');
+
+  // Use gray-matter to parse the post metadata section
+  const matterResult = matter(fileContents);
+
+  // Combine the data with the id
+  return {
+    id,
+    ...matterResult.data,
+  };
 }
